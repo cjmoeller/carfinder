f4531e577f30154a970f49f82af587abf3d565bb
Julius Möller <julius.moeller@uni-oldenburg.de>
2018-11-09 18:43:06 +0100

    Started with bluetooth stuff

---
+++ app/src/main/java/de/uni_oldenburg/carfinder/bluetooth/BluetoothChangeReceiver.java
@@ -0,0 +1,15 @@
+package de.uni_oldenburg.carfinder.bluetooth;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Receives Broadcast for bluetooth events such as 'device connected' and 'device disconnected'.
+ */
+public class BluetoothChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/de/uni_oldenburg/carfinder/fragments/SettingsFragment.java
+++ app/src/main/java/de/uni_oldenburg/carfinder/fragments/SettingsFragment.java
@@ -1,7 +1,13 @@
 package de.uni_oldenburg.carfinder.fragments;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import androidx.preference.MultiSelectListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import de.uni_oldenburg.carfinder.R;
 
@@ -10,5 +16,32 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings);
+        MultiSelectListPreference bluetoothDevices = (MultiSelectListPreference) findPreference("pref_key_bluetooth_device");
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            bluetoothDevices.setEnabled(false);
+            return;
+        }
+
+        ArrayList<BluetoothDevice> pairedDevices = new ArrayList<>(bluetoothAdapter.getBondedDevices());
+        //TODO: Check if bluetooth on. When its not ask the user to turn it on.
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            String[] entries = new String[pairedDevices.size()];
+            String[] values = new String[pairedDevices.size()];
+
+            for (int i = 0; i < pairedDevices.size(); i++) {
+                String deviceName = pairedDevices.get(i).getName();
+                String deviceHardwareAddress = pairedDevices.get(i).getAddress(); // MAC address
+                entries[i] = deviceName;
+                values[i] = deviceHardwareAddress;
+            }
+
+            bluetoothDevices.setEntries(entries);
+            bluetoothDevices.setEntryValues(values);
+        } else {
+            bluetoothDevices.setEnabled(false);
+            return;
+        }
     }
 }
